4b8763e617b314505ac048e9e9185fc2b000111a
NarendraXD <narendraahirwar20038@gmail.com>
2026-06-22 18:30:47 +0530

    Added auth routes and fixed backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,8 +12,13 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/survey", require("./routes/survey"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // https://survey-frontend-i7yg33tty-narendra-xd-s-projects.vercel.app/
