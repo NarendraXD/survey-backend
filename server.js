efe0fca6e9310985bdd490db3dde7cf7655b19b4
NarendraXD <narendraahirwar20038@gmail.com>
2026-06-23 12:41:04 +0530

    fix cors

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
-app.use('/api/survey', require('./routes/survey'));
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use("/api/auth", require("./routes/auth"));
 app.use("/api/survey", require("./routes/survey"));
+app.use("/api/auth", require("./routes/auth"));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-// https://survey-frontend-i7yg33tty-narendra-xd-s-projects.vercel.app/
+app.listen(5000, () => console.log("Server running on port 5000"));
